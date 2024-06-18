fc8818ea454fc68b7dbbe9b9928730adb41f8877
Drew Zemke <drew.zemke@gmail.com>
2024-06-18 08:16:15 -0700

    refactor(errors): better handling, dealt with all `unwrap`s

--- src/app/state.rs
+++ src/app/state.rs
@@ -1,18 +1,25 @@
 #![allow(clippy::struct_field_names)]
 use crate::tree::top_level_document;
 use futures::TryStreamExt;
-use mongodb::{bson::Bson, options::FindOptions, Client, Database};
+use mongodb::{
+    bson::Bson,
+    options::FindOptions,
+    results::{CollectionSpecification, DatabaseSpecification},
+    Client, Database,
+};
 use ratatui::widgets::ListState;
 use std::sync::mpsc::{self, Receiver, Sender};
 use tui_tree_widget::{TreeItem, TreeState};
 
 const PAGE_SIZE: usize = 5;
+const SEND_ERR_MSG: &str = "Error occurred while processing server response.";
 
 pub enum MongoResponse {
     Query(Vec<Bson>),
-    DbNames(Vec<String>),
-    CollectionNames(Vec<String>),
+    Databases(Vec<DatabaseSpecification>),
+    Collections(Vec<CollectionSpecification>),
     Count(u64),
+    Error(String),
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -80,19 +87,12 @@ impl<'a> State<'a> {
         let client = self.client.clone();
 
         tokio::spawn(async move {
-            let result: Vec<String> = client
-                .list_databases(None, None)
-                .await
-                .unwrap()
-                .iter()
-                .map(|db| db.name.clone())
-                .collect();
+            let response: MongoResponse = client.list_databases(None, None).await.map_or_else(
+                |err| MongoResponse::Error(err.to_string()),
+                MongoResponse::Databases,
+            );
 
-            // FIXME: Need a way (maybe another channel) to communicate to the UI
-            // that the sync failed
-            sender
-                .send(MongoResponse::DbNames(result))
-                .expect("Error occurred while processing server response.");
+            sender.send(response).expect(SEND_ERR_MSG);
         });
     }
 
@@ -110,22 +110,15 @@ impl<'a> State<'a> {
         let sender = self.query_send.clone();
 
         tokio::spawn(async move {
-            let result: Vec<String> = db
-                .list_collections(None, None)
-                .await
-                .unwrap()
-                .try_collect::<Vec<_>>()
-                .await
-                .unwrap()
-                .iter()
-                .map(|coll| coll.name.clone())
-                .collect();
+            let resonse = match db.list_collections(None, None).await {
+                Ok(cursor) => cursor.try_collect::<Vec<_>>().await.map_or_else(
+                    |err| MongoResponse::Error(err.to_string()),
+                    MongoResponse::Collections,
+                ),
+                Err(err) => MongoResponse::Error(err.to_string()),
+            };
 
-            // FIXME: Need a way (maybe another channel) to communicate to the UI
-            // that the sync failed
-            sender
-                .send(MongoResponse::CollectionNames(result))
-                .expect("Error occurred while processing server response.");
+            sender.send(resonse).expect(SEND_ERR_MSG);
         });
     }
 
@@ -156,20 +149,19 @@ impl<'a> State<'a> {
         options.limit = Some(PAGE_SIZE as i64);
 
         tokio::spawn(async move {
-            let result: Vec<Bson> = db
+            let cursor = db
                 .collection::<Bson>(&collection_name)
                 .find(None, options)
-                .await
-                .unwrap()
-                .try_collect::<Vec<Bson>>()
-                .await
-                .unwrap();
+                .await;
+            let response = match cursor {
+                Ok(cursor) => cursor.try_collect::<Vec<_>>().await.map_or_else(
+                    |err| MongoResponse::Error(err.to_string()),
+                    MongoResponse::Query,
+                ),
+                Err(err) => MongoResponse::Error(err.to_string()),
+            };
 
-            // FIXME: Need a way (maybe another channel) to communicate to the UI
-            // that the sync failed
-            sender
-                .send(MongoResponse::Query(result))
-                .expect("Error occurred while processing server response.");
+            sender.send(response).expect(SEND_ERR_MSG);
         });
     }
 
@@ -194,17 +186,16 @@ impl<'a> State<'a> {
         let sender = self.query_send.clone();
 
         tokio::spawn(async move {
-            let count = db
+            let response = db
                 .collection::<Bson>(&collection_name)
                 .count_documents(None, None)
                 .await
-                .unwrap();
+                .map_or_else(
+                    |err| MongoResponse::Error(err.to_string()),
+                    MongoResponse::Count,
+                );
 
-            // FIXME: Need a way (maybe another channel) to communicate to the UI
-            // that the sync failed
-            sender
-                .send(MongoResponse::Count(count))
-                .expect("Error occurred while processing server response.");
+            sender.send(response).expect(SEND_ERR_MSG);
         });
     }
 
@@ -213,7 +204,7 @@ impl<'a> State<'a> {
             MongoResponse::Query(content) => {
                 let items: Vec<TreeItem<String>> = content
                     .iter()
-                    .map(|x| top_level_document(x.as_document().unwrap()))
+                    .filter_map(|bson| bson.as_document().map(top_level_document))
                     .collect();
 
                 // initial state has all top-level documents expanded
@@ -226,11 +217,14 @@ impl<'a> State<'a> {
                 self.main_view_state = state;
             }
             MongoResponse::Count(count) => self.count = count,
-            MongoResponse::DbNames(db_names) => self.db_names = db_names,
-            MongoResponse::CollectionNames(coll_names) => {
-                self.coll_names = coll_names;
+            MongoResponse::Databases(dbs) => {
+                self.db_names = dbs.iter().map(|db| db.name.clone()).collect();
+            }
+            MongoResponse::Collections(colls) => {
+                self.coll_names = colls.iter().map(|coll| coll.name.clone()).collect();
                 self.coll_list_state.select(None);
             }
+            MongoResponse::Error(_) => todo!("Need to implement better handling."),
         };
         self.new_data = true;
     }
