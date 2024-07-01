82088eeb7d6023bc1f8e7cf3bebde6c5a03bc11a
Drew Zemke <drew.zemke@gmail.com>
2024-06-30 22:49:48 -0700

    chore(package): version bump

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ let
 in
 rust.buildRustPackage rec {
   pname = "mongodb-tui";
-  version = "0.4.0"; 
+  version = "0.5.0"; 
 
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
