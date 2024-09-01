d5630b9c7f1bf9ff48ebe2938b500be8c2225b5a
Drew Zemke <drew.zemke@gmail.com>
2024-09-01 11:54:28 -0700

    feat(edit-docs): better error handling

--- src/utils/edit_doc.rs
+++ src/utils/edit_doc.rs
@@ -1,4 +1,3 @@
-use anyhow::{Context, Result};
 use crossterm::{
     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
@@ -7,21 +6,52 @@ use edit::{edit_with_builder, Builder};
 use mongodb::bson::{from_document, to_document, Document};
 use serde_json::{from_str, Value};
 use std::io::stdout;
+use thiserror::Error;
 
-// TODO: better error handling
+#[derive(Error, Debug)]
+pub enum EditDocError {
+    #[error("Could not convert doc to JSON: {0}")]
+    DocToJson(#[from] mongodb::bson::de::Error),
+
+    #[error("Could not convert doc to JSON: {0}")]
+    JsonToString(serde_json::Error),
+
+    #[error("Error editing string: {0}")]
+    EditString(#[from] std::io::Error),
+
+    #[error("Could not parse JSON: {0}")]
+    StringToJson(serde_json::Error),
+
+    #[error("Could not convet JSON in to doc: {0}")]
+    JsonToDoc(#[from] mongodb::bson::ser::Error),
+
+    #[error("Terminal error occurred: {0}")]
+    TerminalCommand(std::io::Error),
+}
+
 /// Edit document in external editor
-pub fn edit_doc(doc: Document) -> Result<Document> {
+pub fn edit_doc(doc: Document) -> Result<Document, EditDocError> {
+    // prepare doc
     let doc_string = from_document::<Value>(doc)
-        .context("converting doc to json")
-        .and_then(|json| {
-            serde_json::to_string_pretty(&json).context("converting json to string")
-        })?;
+        .map_err(EditDocError::DocToJson)
+        .and_then(|json| serde_json::to_string_pretty(&json).map_err(EditDocError::JsonToString))?;
 
-    stdout().execute(LeaveAlternateScreen)?;
-    let updated_string =
-        edit_with_builder(doc_string, Builder::new().suffix(".json")).context("editing string")?;
-    stdout().execute(EnterAlternateScreen)?;
+    // setup terminal for external editor
+    stdout()
+        .execute(LeaveAlternateScreen)
+        .map_err(EditDocError::TerminalCommand)?;
 
-    let new_doc_json = from_str::<Value>(&updated_string).context("converting string to json")?;
-    to_document(&new_doc_json).context("converting json to doc")
+    // call function from `edit` crate
+    let updated_string = edit_with_builder(doc_string, Builder::new().suffix(".json"))
+        .map_err(EditDocError::EditString)?;
+
+    // setup terminal for returning to app
+    stdout()
+        .execute(EnterAlternateScreen)
+        .map_err(EditDocError::TerminalCommand)?;
+
+    // convert back into a doc
+    let new_doc_json = from_str::<Value>(&updated_string).map_err(EditDocError::StringToJson)?;
+
+    to_document(&new_doc_json).map_err(EditDocError::JsonToDoc)
 }
