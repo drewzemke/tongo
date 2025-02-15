c5ac3effe538a4c5460e4bf7420c0632fd7bac7a
Erik <ewaasen@ucdavis.edu>
2025-02-14 16:06:04 -0800

    chore: download project with nix

--- default.nix
+++ default.nix
@@ -1,12 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/0ff09db9d034a04acd4e8908820ba0b410d7a33a.tar.gz") {} }:
 
-let
-  rust = pkgs.rustPlatform;
-in
-rust.buildRustPackage rec {
-  pname = "tongo";
-  version = "0.10.0"; 
-
-  cargoLock.lockFile = ./Cargo.lock;
-  src = pkgs.lib.cleanSource ./.;
+pkgs.mkShell {
+  packages = with pkgs; [
+    pkgs.cargo
+    pkgs.git
+    pkgs.just
+  ];
 }
+
