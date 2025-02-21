695b867be7204dd034fb131b41c7eafb2567927b
Drew Zemke <drew.zemke@gmail.com>
2025-02-20 19:23:15 -0800

    refactor(connections): connections have ids

--- src/connection.rs
+++ src/connection.rs
@@ -1,14 +1,18 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize, PartialEq, Eq)]
 pub struct Connection {
+    #[serde(default = "Uuid::new_v4")]
+    id: Uuid,
     pub name: String,
     pub connection_str: String,
 }
 
 impl Connection {
-    pub const fn new(name: String, connection_str: String) -> Self {
+    pub fn new(name: String, connection_str: String) -> Self {
         Self {
+            id: Uuid::new_v4(),
             name,
             connection_str,
         }
