11aa8237485307e2a24467fd657996f55adcbad5
Drew Zemke <drew.zemke@gmail.com>
2025-03-15 14:47:03 -0700

    feat(status-bar): show app name and version in the bottom right corner

--- src/components/status_bar.rs
+++ src/components/status_bar.rs
@@ -1,8 +1,3 @@
-use std::{
-    rc::Rc,
-    time::{Duration, Instant},
-};
-
 use crate::{
     components::Component,
     key_map::KeyMap,
@@ -12,9 +7,10 @@ use crate::{
         Signal,
     },
 };
-use ratatui::{
-    prelude::*,
-    widgets::{Block, Padding, Paragraph, Wrap},
+use ratatui::prelude::*;
+use std::{
+    rc::Rc,
+    time::{Duration, Instant},
 };
 
 const DEBUG_RENDER_COUNT: bool = false;
@@ -88,44 +84,43 @@ impl StatusBar {
 
 impl Component for StatusBar {
     fn render(&mut self, frame: &mut Frame, area: Rect) {
-        let content = self.message.as_ref().map_or_else(
-            || {
-                Line::from(
-                    self.command_manager
-                        .groups()
-                        .into_iter()
-                        .filter(|group| group.category == CommandCategory::StatusBarOnly)
-                        .flat_map(|group| self.key_map.cmd_group_to_span(&group))
-                        .collect::<Vec<Span>>(),
-                )
-            },
-            |message| {
-                let (prefix, style) = match message.kind {
-                    MessageKind::Error => ("● Error: ", Style::default().red()),
-                    MessageKind::Info => ("● ", Style::default().blue()),
-                    MessageKind::Success => ("● Success: ", Style::default().green()),
-                };
-                Line::from(vec![
-                    Span::styled(prefix, style),
-                    Span::from(message.content.clone()),
-                ])
-            },
-        );
+        // render the message if there is one, otherwise show commands and app name
+        if let Some(message) = &self.message {
+            let (prefix, style) = match message.kind {
+                MessageKind::Error => ("● Error: ", Style::default().red()),
+                MessageKind::Info => ("● ", Style::default().blue()),
+                MessageKind::Success => ("● Success: ", Style::default().green()),
+            };
+            let content = Line::from(vec![
+                Span::styled(prefix, style),
+                Span::from(message.content.clone()),
+            ]);
 
-        let content = Paragraph::new(content)
-            .wrap(Wrap::default())
-            .block(Block::default().padding(Padding::horizontal(1)));
-
-        if DEBUG_RENDER_COUNT {
-            self.renders += 1;
-            let render_count_content = Paragraph::new(format!("{}", &self.renders));
-
-            let layout =
-                Layout::horizontal([Constraint::Fill(1), Constraint::Length(4)]).split(area);
-            frame.render_widget(content, layout[0]);
-            frame.render_widget(render_count_content, layout[1]);
+            frame.render_widget(content, area.inner(Margin::new(1, 0)));
         } else {
-            frame.render_widget(content, area);
+            let layout = Layout::horizontal([Constraint::Fill(1), Constraint::Length(16)])
+                .horizontal_margin(1)
+                .split(area);
+
+            let commands = Line::from(
+                self.command_manager
+                    .groups()
+                    .into_iter()
+                    .filter(|group| group.category == CommandCategory::StatusBarOnly)
+                    .flat_map(|group| self.key_map.cmd_group_to_span(&group))
+                    .collect::<Vec<Span>>(),
+            );
+
+            let right_content = if DEBUG_RENDER_COUNT {
+                self.renders += 1;
+                Line::from(format!("{}", &self.renders)).right_aligned()
+            } else {
+                Line::from(format!("tongo v{}", env!("CARGO_PKG_VERSION")).magenta())
+                    .right_aligned()
+            };
+
+            frame.render_widget(commands, layout[0]);
+            frame.render_widget(right_content, layout[1]);
         }
     }
 
